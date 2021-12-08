11d97265f16db429190f677b09f5cb50fdf2ac15
David Wagih <davidwagih62@gmail.com>
2021-12-08 20:39:12 +0200

    Implementing the Browse Button event

--- XMLEditor/MainWindow.xaml.cs
+++ XMLEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Xml;
+using System.IO;
 namespace XMLEditor
 {
     /// <summary>
@@ -27,7 +29,37 @@ namespace XMLEditor
 
         private void Browse_Click(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("hello dazy");
+
+            // open the dialogue to choose your XML File
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Title = "Browse XML File",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "xml",
+                Filter = "xml files (*.xml)|*.xml",
+                
+            };
+            openFileDialog1.ShowDialog();
+            string filePath = openFileDialog1.FileName; // getting the full file path of the selected XML file
+
+            // load the XML file content from the file the user selected, we will need the file path of the selected one.
+
+            string fileContent = File.ReadAllText(filePath); // fileContent contains the content of our selected file finally..
+
+            // we then want to change the content of the input GUI element into the fileContent...
+
+            inputField.Text = fileContent;
+
+
+        }
+
+        // want to be able to save the file as xml file or JSON file
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
