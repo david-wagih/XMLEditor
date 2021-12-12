3738c565e24de76c9e171c1aff9141c2fbf8744c
Marina-Ehab-Youssef <marinaehabyoussef1@gmail.com>
2021-12-12 20:45:26 +0200

    added convert to json and tree

---
+++ XMLEditor/ConvertToJSON.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLEditor
+{
+    class ConvertToJSON
+    {
+        public int first;
+        public string filename;
+
+        public ConvertToJSON(string fname, int first)
+        {
+            filename = fname;
+            this.first = first;
+        }
+
+        //Write: writes the passed string in the file, if add is true it adds new line after writing
+        public void Write(string s, bool add)
+        {
+            if (first == 0)
+            {
+                StreamWriter sw = new StreamWriter(filename, false);
+                if (add == true)
+                {
+                    sw.WriteLine(s);
+                    sw.Close();
+                }
+                else
+                {
+                    sw.Write(s);
+                    sw.Close();
+                }
+                first = 1;
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(filename, true);
+                if (add == true)
+                {
+                    sw.WriteLine(s);
+                    sw.Close();
+                }
+                else
+                {
+                    sw.Write(s);
+                    sw.Close();
+                }
+            }
+        }
+        public void Convert(Node node, Node parent)
+        {
+            List<Node> children = new List<Node>();
+            children = node.getChildren();
+            Stack<string> syntax = new Stack<string>();
+            string space = " ";
+            string indentation = "    ";
+            string openCurlyBraces = "{";
+            string closedCurlyBraces = "}";
+            string openSquareBrackets = "[";
+            string closedSquareBrackets = "]";
+            //base case
+            if (node == null)
+            {
+                return;
+            }
+            //adding identation
+            for (int i = 0; i < parent.getDepth(); i++)
+            {
+                Write(indentation, false);
+                syntax.Push(indentation);
+            }
+            //if node is the root
+            if (node.getDepth() == 0)
+            {
+                Write(openCurlyBraces, true);
+                syntax.Push(openCurlyBraces);
+                Write('"' + node.getTagName() + '"' + ':' + space, true);
+
+            }
+            else
+            { //if node has "siblings" of the same tag name (array) 
+                if (parent.getChildren().Count() > 1 && node.getTagName() == parent.getChildren()[1].getTagName())
+                {
+                    if (node == parent.getChildren()[0])
+                    {
+                        Write(openCurlyBraces, true);
+                        syntax.Push(openCurlyBraces);
+                        if (children.Count == 0)
+                        {
+                            Write('"' + node.getTagName() + '"' + ":" + space + openSquareBrackets + '"' + node.getTagValue() + '"', true);
+                        }
+                        else
+                        {
+                            Write('"' + node.getTagName() + '"' + ":" + space + openSquareBrackets, true);
+                        }
+                        syntax.Push(openSquareBrackets);
+
+                    }
+                    else if (node != parent.getChildren()[0] && children.Count > 0)
+                    {
+                        Write(',' + openCurlyBraces, true);
+                        syntax.Push(openCurlyBraces);
+                    }
+                    else if (node != parent.getChildren()[0] && children.Count == 0)
+                    {
+                        Write(',' + '"' + node.getTagValue() + '"', true);
+                    }
+                }
+                else if (children.Count() == 0)
+                {
+                    // if node has siblings and no children
+                    if (parent.getChildren().Count() > 1)
+                    {
+                        if (node == parent.getChildren()[0])
+                        {
+                            Write(openCurlyBraces + '"' + node.getTagName() + '"' + ':' + space + '"' + node.getTagValue() + '"', true);
+                        }
+                        else if (node != parent.getChildren()[0])
+                        {
+                            Write(',' + openCurlyBraces + '"' + node.getTagName() + '"' + ':' + space + '"' + node.getTagValue() + '"', true);
+                        }
+
+                    }
+                    else
+                    {
+                        //if node has no children and no siblings
+                        Write(openCurlyBraces + '"' + node.getTagName() + '"' + ':' + space + '"' + node.getTagValue() + '"', true);
+                    }
+                    syntax.Push(openCurlyBraces);
+                }
+                else if (children.Count() > 0)
+                {
+                    //if it has children and siblings
+                    if (parent.getChildren().Count() > 1)
+                    {
+                        if (node == parent.getChildren()[0])
+                        {
+                            Write(openCurlyBraces + '"' + node.getTagName() + '"' + ':' + space, true);
+                        }
+                        else if (node != parent.getChildren()[0])
+                        {
+                            Write(',' + openCurlyBraces + '"' + node.getTagName() + '"' + ':' + space, true);
+                        }
+
+                    }
+                    else
+                    {
+                        //if node has children and no siblings
+                        Write(openCurlyBraces + '"' + node.getTagName() + '"' + ':' + space, true);
+                    }
+                    syntax.Push(openCurlyBraces);
+                }
+
+            }
+
+
+            foreach (Node child in children)
+            {
+                Convert(child, node);
+            }
+
+            while (syntax.Count > 0)
+            {
+                if (syntax.Peek() == openCurlyBraces)
+                {
+                    Write(closedCurlyBraces, true);
+                    syntax.Pop();
+                }
+                else if (syntax.Peek() == openSquareBrackets)
+                {
+                    Write(closedSquareBrackets, true);
+                }
+                else
+                {
+                    Write(syntax.Pop(), false);
+                }
+            }
+
+        }
+    }
+}
--- XMLEditor/Tree.cs
+++ XMLEditor/Tree.cs
@@ -6,8 +6,244 @@ using System.Threading.Tasks;
 
 namespace XMLEditor
 {
+    class Node
+    {
+        private string tagName;
+        private string tagValue;
+        private string tagAttributes;
+        private int depth;
+        private bool isClosingTag;
+        private List<Node> children = new List<Node>();
+
+        public Node(string tagName, string tagValue, string tagAttributes, bool isClosingTag, int depth)
+        {
+            this.tagName = tagName;
+            this.tagValue = tagValue;
+            this.depth = depth;
+            this.tagAttributes = tagAttributes;
+            this.isClosingTag = isClosingTag;
+        }
+
+        public Node()
+        {
+            tagName = null;
+            tagValue = null;
+            tagAttributes = null;
+            isClosingTag = false;
+            depth = 0;
+        }
+
+        public List<Node> getChildren()
+        {
+            return children;
+        }
+
+        public string getTagName()
+        {
+            return tagName;
+        }
+
+        public string getTagValue()
+        {
+            return tagValue;
+        }
+
+        public int getDepth()
+        {
+            return depth;
+        }
+
+        public string getTagAttributes()
+        {
+            return tagAttributes;
+        }
+        public bool getIsClosingTag()
+        {
+            return isClosingTag;
+        }
+
+        public void setTagName(string tn)
+        {
+            tagName = tn;
+        }
+        public void setTagValue(string tv)
+        {
+            tagValue = tv;
+        }
+        public void setIsClosingTag(bool check)
+        {
+            isClosingTag = check;
+        }
+        public void setDepth(int d)
+        {
+            depth = d;
+        }
+
+        public void setTagAttributes(string ta)
+        {
+            tagAttributes = ta;
+        }
+    }
+
     class Tree
     {
+        private Node root;
+        public Tree(Node root)
+        {
+            this.root = root;
+        }
 
+        public Tree()
+        {
+            root = null;
+        }
+
+        private char skipSpaces(StreamReader reader)
+        {
+            char letter = (char)reader.Read();
+            while (letter == '\n' || letter == '\t') letter = (char)reader.Read();
+            return letter;
+        }
+
+        public Node getTreeRoot()
+        {
+            return this.root.getChildren()[0];
+        }
+
+        private void insertFileAUX(StreamReader reader, Node parent)
+        {
+            char letter;
+            string data;
+            string name;
+
+            while (reader.Peek() >= 0)
+            {
+                //read one char skipping spaces & new lines
+                letter = skipSpaces(reader);
+
+                //check if we are reading an opening tag
+                if (letter == '<' && reader.Peek() != (int)('/'))
+                {
+                    letter = skipSpaces(reader);
+
+                    if (Char.IsLetter(letter))
+                    {
+                        //add new child to current parent and update child depth
+                        Node child = new Node(null, null, null, false, parent.getDepth() + 1);
+                        parent.getChildren().Add(child);
+
+                        //save the tagName in name
+                        name = letter.ToString();
+                        letter = (char)reader.Read();
+
+                        while (letter != '>')
+                        {
+                            name += letter.ToString();
+                            letter = (char)reader.Read();
+
+                            //save attributes
+                            if (letter == ' ')
+                            {
+                                child.setTagName(name);
+                                data = ((char)reader.Read()).ToString();
+                                letter = (char)reader.Read();
+                                while (letter != '>')
+                                {
+                                    //check for self closing tag
+                                    if (letter == '/' && reader.Peek() == (int)('>'))
+                                    {
+                                        child.setIsClosingTag(true);
+                                        child.setTagAttributes(data);
+                                        letter = (char)reader.Read();
+                                        continue;
+                                    }
+
+                                    data += letter.ToString();
+
+                                    letter = (char)reader.Read();
+                                }
+
+                                child.setTagAttributes(data);
+                            }
+
+                            //check for self closing tag
+                            if (letter == '/' && reader.Peek() == (int)('>'))
+                            {
+                                child.setIsClosingTag(true);
+                                child.setTagName(name);
+                                letter = (char)reader.Read();
+                                continue;
+                            }
+                        }
+
+                        //****save the tagname in node****
+                        child.setTagName(name);
+
+                        //skip spaces & new lines without consuming the letter
+                        while (reader.Peek() == (int)('\n') || reader.Peek() == (int)('\t') || reader.Peek() == (int)(' '))
+                        {
+                            letter = (char)reader.Read();
+                        }
+
+                        //save the data inside the tag if there is data
+                        if (reader.Peek() != (int)('<'))
+                        {
+                            letter = (char)reader.Read();
+                            data = letter.ToString();
+
+                            //save the data until reaching the closing tag
+                            while (reader.Peek() != (int)('<'))
+                            {
+                                letter = (char)reader.Read();
+                                data += letter.ToString();
+                            }
+
+                            //remove any /n from the end of the data before saving it 
+                            int index = data.Length;
+                            while (data[index - 1] == '\n' || data[index - 1] == ' ' || data[index - 1] == '\t')
+                            {
+                                data = data.Substring(0, index - 1);
+                                index = data.Length;
+                            }
+                            //****save the data in node****
+                            child.setTagValue(data);
+
+                        }
+
+                        else if (child.getIsClosingTag() == false)
+                        {
+                            //this child also has children
+                            insertFileAUX(reader, child);
+                        }
+
+                    }
+                }
+
+                //check if we are reading a closing tag for that parent
+                else if (letter == '<' && reader.Peek() == (int)('/'))
+                {
+                    letter = (char)reader.Read();
+                    letter = (char)reader.Read();
+                    name = letter.ToString();
+                    letter = (char)reader.Read();
+                    while (letter != '>')
+                    {
+                        name += letter.ToString();
+                        letter = (char)reader.Read();
+                    }
+
+                    if (name == parent.getTagName())
+                        return;
+                }
+            }
+        }
+
+        public void insertFile(StreamReader reader)
+        {
+            Node parent = new Node();
+            root = parent;
+            parent.setDepth(-1);
+            insertFileAUX(reader, parent);
+        }
     }
 }
