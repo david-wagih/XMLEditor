a256dc416aab1e8323b49dda78429f409187813b
Mario-Kamel <85037953+Mario-Kamel@users.noreply.github.com>
2021-12-19 01:24:59 +0200

    Yarab toub 3alena
    :)

--- XMLEditor/Fix.cs
+++ XMLEditor/Fix.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Text.RegularExpressions;
 using System.Text;
 
-namespace XMLEditor
+namespace ConsoleApp2
 {
     /************************************************************************************************************************************************/
     /*Known bugs: 
@@ -13,7 +13,8 @@ namespace XMLEditor
      * 
      *
      *
-     * 
+     * Rooms for Improvement:
+     * instead of a tempTag, tags are popped into another stack until the tag is found then reverted to the original stack (benefit: allows the correction of multiple consecutive tags)
      * 
      * 
      * 
@@ -32,19 +33,22 @@ namespace XMLEditor
      * 
      * 
      */
-    class Fix
+    class Program
     {
-        public string[] validator(string path)
+        static string[] validator()
         {
             bool missingOpeningTags = false;
             bool missingEndTags = false;
+            int lastOpenTagPoppedLine = 0;
+            int lastEndTagFound = 0;
             //bool missingOpenBrackets = false;
             Stack tags = new Stack();
             Stack tagsLocation = new Stack();
             Stack brackets = new Stack() ;
             Stack bracketsLocation = new Stack();
+            
 
-            string[] lines = System.IO.File.ReadAllLines(path); //file directory, DAVIIIIID
+            string[] lines = System.IO.File.ReadAllLines(@"D:\Engineering\Senior 1\Data Structures and Algorithms\Trials\Text.txt"); //file directory, DAVIIIIID
             for (int i = 0; i < lines.Length; i++)
             {
                 string line = lines[i];
@@ -73,9 +77,9 @@ namespace XMLEditor
                             }
                             else    //closing bracket is missing
                             {
-                                lines[i] += ">";
+                                lines[i] = line.Substring(0, j + cnt) + ">" + line.Substring(j + cnt, line.Length - 1);     //Should replace all the + "</"
                                 line = lines[i];
-                                Console.WriteLine("Adding a closing tag at the end of line " + i);
+                                Console.WriteLine("Adding a closing bracket at the end of line " + i);
                                 tag = line.Substring(j + 2, cnt - 1);
                                 j += cnt - 1;
 
@@ -85,7 +89,11 @@ namespace XMLEditor
                                 if (tags.Peek().ToString() == tag) //matching end-tag with open-tag in stack
                                 {
                                     tags.Pop();
+                                    lastEndTagFound = i;
+                                    int[] boo = (int[])tagsLocation.Peek();
+                                    lastOpenTagPoppedLine = boo[0];
                                     tagsLocation.Pop();
+
                                 }
                                 else //missing open-tag or next opening tag
                                 {
@@ -98,14 +106,18 @@ namespace XMLEditor
                                         if (tags.Peek().ToString() == tag)
                                         {
                                             Console.WriteLine("The tag " + tempTag + " in the line " + (int)(tempLocation[0]) + " and starting from " + (int)(tempLocation[1]) + " with length of " + (int)(tempLocation[2]) + " has its end-tag missing");
-                                            missingEndTags = true;
+                                            lines[i] = "</" + tempTag + ">" + lines[i];     //MODIFIED REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+                                            //missingEndTags = true;
+                                            lastEndTagFound = i; //QUESTIONABLE
                                             tags.Pop();
                                             tagsLocation.Pop();
                                         }
                                         else
                                         {
 
-                                            Console.WriteLine("The tag " + tag + " in the line " + (int)(i + 1) + " and starting from " + (int)(j - cnt + 1) + " with length of " + (int)(cnt - 2) + " has its open-tag missing");
+                                            Console.WriteLine("The tag " + tag + " in the line " + i + " and starting from " + (int)(j - cnt + 1) + " with length of " + (int)(cnt - 2) + " has its open-tag missing");
+                                            lines[tempLocation[0]] += " <" + tag + ">";     //MODIFIED REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
+                                            
                                             missingOpeningTags = true;
                                             tags.Push(tempTag);
                                             tagsLocation.Push(tempLocation);
@@ -114,7 +126,10 @@ namespace XMLEditor
                                     else    //stack is empty
                                     {
                                         missingOpeningTags = true;
-                                        Console.WriteLine("The tag " + tag + " in the line " + (int)(i + 1) + " and starting from " + (int)(j + 2) + " with length of " + (int)(cnt - 2) + " has its open-tag missing");
+                                        Console.WriteLine("The tag " + tag + " in the line " + i + " and starting from " + (int)(j + 2) + " with length of " + (int)(cnt - 2) + " has its open-tag missing");
+                                        
+                                        int toBeClosedTagLocation = tempLocation[0];
+                                        lines[toBeClosedTagLocation] = lines[toBeClosedTagLocation] + "<" + tag + ">"  ;
                                         tags.Push(tempTag);
                                         tagsLocation.Push(tempLocation);
                                     }
@@ -124,7 +139,7 @@ namespace XMLEditor
                             {
                                 missingOpeningTags = true;
                                 Console.WriteLine("The tag " + tag + " in the line " + (int)(i+1) + " and starting from " + (int)(j+2) + " with length of " + (int)(cnt-2) + " has its open-tag missing");
-
+                                lines[lastOpenTagPoppedLine] = "<" + tag + ">" + lines[lastOpenTagPoppedLine];      // MODIFIED REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
                             }
                         }
                         else //it's an opening tag
@@ -147,10 +162,12 @@ namespace XMLEditor
                             }
                             else //closing bracket is missing :(
                             {
-                                lines[i] += ">";
+                                lines[i] = line.Substring(0, j + cnt) + ">" + line.Substring(j + cnt, line.Length-cnt);
+                                brackets.Pop();
+                                bracketsLocation.Pop();
                                 line = lines[i];
-                                Console.WriteLine("Adding a closing tag at the end of line " + i);
-                                string tag = line.Substring(j + 1, cnt);
+                                Console.WriteLine("Adding a closing bracket at the end of line " + i);
+                                string tag = line.Substring(j + 1, cnt-1);
                                 tags.Push(tag);
                                 int[] tagLocation = new int[3];
                                 tagLocation[0] = i;
@@ -162,7 +179,7 @@ namespace XMLEditor
                             }
                         }
                     }
-                    else if(line[j] == '>')
+                    else if(line[j] == '>') //closing bracket
                     {
                         if(brackets.Count != 0) //checking that the stack has brackets in order to be able to peek
                         {
@@ -189,7 +206,7 @@ namespace XMLEditor
                             StringBuilder str = new StringBuilder(line);
                             if (j == 0)
                             {
-                                line = "<" + line;
+                                line = "<" + line;  
                             }
                             else
                             {
@@ -211,7 +228,8 @@ namespace XMLEditor
                     string tempTag = (String)tags.Peek();
                     tags.Pop();
                     tagsLocation.Pop();
-                    Console.WriteLine("The tag " + tempTag + " in the line " + (int)(tempLocation[0]) + " and starting from " + (int)(tempLocation[1]) + " with length of " + (int)(tempLocation[2]) + " has its end-tag missing");
+                    Console.WriteLine("The tag " + tempTag + " in the line " + (int)(tempLocation[0]) + " and starting from " + (int)(tempLocation[1]) + " with length of " + (int)(tempLocation[2]) + " has its end-tag missing"); //Users missing
+                    lines[lastEndTagFound] += " </" + tempTag + ">";        //MODIFIED REEEEEEEEEEEEEEEEEEEEEEEEEEEE
 
                 }
                 missingOpeningTags = true;
@@ -223,6 +241,12 @@ namespace XMLEditor
                 Console.WriteLine(lines[i]);
             }
             return lines;
+        }
+
+
+        static void Main(string[] args)
+        {
+            validator();
         }
     }
 }
